40a6cda4511dca771d71b8e44b14f49c9ed99816
VuvuQ <159018145+VuvuQ@users.noreply.github.com>
2026-01-28 14:29:00 +0200

    Update app title and author information

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ Created on Wed Jan 28 10:16:50 2026
 
 import streamlit as st
 
-st.write("Vuyiseka Qomfo Streamlit App")
+st.title("Vuyiseka Qomfo's Personal Research App")
 
 st.write("Day3")
 
-st.title("My first streamlit App")
+st.write("My first streamlit App")
 
 number = st.slider("pick an number", 1, 100)
 
 
 st.write(f"you picked {number}")
+
